2d35ddbb43f678bbcdacb196465f259eff2865be
skuzzis <andreieurt@gmail.com>
2023-11-24 17:32:18 +0200

    update(scripting): Making plugin functions not mandatory

--- plugin_files/scripting/template_plugin/includes/main.h
+++ plugin_files/scripting/template_plugin/includes/main.h
@@ -4,39 +4,41 @@
 #include <stdint.h>
 #include <swiftly/swiftly.h>
 
-void OnPluginStart();
-void OnPluginStop();
+void OnPluginStart() __attribute__((weak));
+void OnPluginStop() __attribute__((weak));
 void OnProgramLoad(const char *pluginName, const char *mainFilePath);
-bool OnClientConnected(Player *player);
-bool OnClientConnect(Player *player);
-void OnPlayerSpawn(Player *player);
-void OnGameTick(bool simulating, bool bFirstTick, bool bLastTick);
-bool OnPlayerChat(Player *player, const char *text, bool teamonly);
-void OnRoundStart(long timelimit, long fraglimit, const char *objective);
-void OnRoundEnd(unsigned char winner, unsigned char reason, const char *message, unsigned char legacy, short player_count, unsigned char nomusic);
-void OnRoundPrestart();
-void OnBombBeginPlant(Player *player, unsigned short site);
-void OnBombAbortPlant(Player *player, unsigned short site);
-void OnBombPlanted(Player *player, unsigned short site);
-void OnBombBeginDefuse(Player *player, unsigned short site);
-void OnBombAbortDefuse(Player *player, unsigned short site);
-void OnBombDefused(Player *player, unsigned short site);
-void OnBombExploded(Player *player, unsigned short site);
-void OnBombDropped(Player *player);
-void OnBombPickup(Player *player);
-void OnMapLoad(const char *mapName);
-void OnMapUnload(const char *mapName);
+bool OnClientConnected(Player *player) __attribute__((weak));
+bool OnClientConnect(Player *player) __attribute__((weak));
+void OnPlayerSpawn(Player *player) __attribute__((weak));
+void OnGameTick(bool simulating, bool bFirstTick, bool bLastTick) __attribute__((weak));
+bool OnPlayerChat(Player *player, const char *text, bool teamonly) __attribute__((weak));
+void OnRoundStart(long timelimit, long fraglimit, const char *objective) __attribute__((weak));
+void OnRoundEnd(unsigned char winner, unsigned char reason, const char *message, unsigned char legacy, short player_count, unsigned char nomusic) __attribute__((weak));
+void OnRoundPrestart() __attribute__((weak));
+void OnBombBeginPlant(Player *player, unsigned short site) __attribute__((weak));
+void OnBombAbortPlant(Player *player, unsigned short site) __attribute__((weak));
+void OnBombPlanted(Player *player, unsigned short site) __attribute__((weak));
+void OnBombBeginDefuse(Player *player, unsigned short site) __attribute__((weak));
+void OnBombAbortDefuse(Player *player, unsigned short site) __attribute__((weak));
+void OnBombDefused(Player *player, unsigned short site) __attribute__((weak));
+void OnBombExploded(Player *player, unsigned short site) __attribute__((weak));
+void OnBombDropped(Player *player) __attribute__((weak));
+void OnBombPickup(Player *player) __attribute__((weak));
+void OnMapLoad(const char *mapName) __attribute__((weak));
+void OnMapUnload(const char *mapName) __attribute__((weak));
 
 extern "C"
 {
     void Internal_OnPluginStart()
     {
         print("");
-        OnPluginStart();
+        if (OnPluginStart)
+            OnPluginStart();
     }
     void Internal_OnPluginStop()
     {
-        OnPluginStop();
+        if (OnPluginStop)
+            OnPluginStop();
     }
     void Internal_OnProgramLoad(const char *pluginName, const char *mainFilePath)
     {
@@ -44,6 +46,9 @@ extern "C"
     }
     bool Internal_OnClientConnected(uint32_t slot)
     {
+        if (!OnClientConnected)
+            return true;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return false;
@@ -52,6 +57,9 @@ extern "C"
     }
     bool Internal_OnClientConnect(uint32_t slot)
     {
+        if (!OnClientConnect)
+            return true;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return false;
@@ -64,13 +72,17 @@ extern "C"
         if (player == nullptr)
             return;
 
-        OnPlayerSpawn(player);
+        if (OnPlayerSpawn)
+            OnPlayerSpawn(player);
 
         if (player->IsFirstSpawn())
             player->SetFirstSpawn(true);
     }
     bool Internal_OnPlayerChat(uint32_t slot, const char *text, bool teamonly)
     {
+        if (!OnPlayerChat)
+            return true;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return false;
@@ -87,23 +99,30 @@ extern "C"
     }
     void Internal_OnGameTick(bool simulating, bool bFirstTick, bool bLastTick)
     {
-        OnGameTick(simulating, bFirstTick, bLastTick);
+        if (OnGameTick)
+            OnGameTick(simulating, bFirstTick, bLastTick);
     }
     void Internal_OnRoundEnd(unsigned char winner, unsigned char reason, const char *message, unsigned char legacy, short player_count, unsigned char nomusic)
     {
-        OnRoundEnd(winner, reason, message, legacy, player_count, nomusic);
+        if (OnRoundEnd)
+            OnRoundEnd(winner, reason, message, legacy, player_count, nomusic);
     }
     void Internal_OnRoundStart(long timelimit, long fraglimit, const char *objective)
     {
-        OnRoundStart(timelimit, fraglimit, objective);
+        if (OnRoundStart)
+            OnRoundStart(timelimit, fraglimit, objective);
     }
     void Internal_OnRoundPrestart()
     {
-        OnRoundPrestart();
+        if (OnRoundPrestart)
+            OnRoundPrestart();
     }
 
     void Internal_BombBeginPlant(uint32_t slot, uint16_t site)
     {
+        if (!OnBombBeginPlant)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -113,6 +132,9 @@ extern "C"
 
     void Internal_BombAbortPlant(uint32_t slot, uint16_t site)
     {
+        if (!OnBombAbortPlant)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -122,6 +144,9 @@ extern "C"
 
     void Internal_BombPlanted(uint32_t slot, uint16_t site)
     {
+        if (!OnBombPlanted)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -131,6 +156,9 @@ extern "C"
 
     void Internal_BombBeginDefuse(uint32_t slot, uint16_t site)
     {
+        if (!OnBombBeginDefuse)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -140,6 +168,9 @@ extern "C"
 
     void Internal_BombAbortDefuse(uint32_t slot, uint16_t site)
     {
+        if (!OnBombAbortDefuse)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -149,6 +180,9 @@ extern "C"
 
     void Internal_BombDefused(uint32_t slot, uint16_t site)
     {
+        if (!OnBombDefused)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -158,6 +192,9 @@ extern "C"
 
     void Internal_BombExploded(uint32_t slot, uint16_t site)
     {
+        if (!OnBombExploded)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -167,6 +204,9 @@ extern "C"
 
     void Internal_BombDropped(uint32_t slot)
     {
+        if (!OnBombDropped)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -176,6 +216,9 @@ extern "C"
 
     void Internal_BombPickup(uint32_t slot)
     {
+        if (!OnBombPickup)
+            return;
+
         Player *player = g_playerManager->GetPlayer(slot);
         if (player == nullptr)
             return;
@@ -185,12 +228,14 @@ extern "C"
 
     void Internal_OnMapLoad(const char *mapName)
     {
-        OnMapLoad(mapName);
+        if (OnMapLoad)
+            OnMapLoad(mapName);
     }
 
     void Internal_OnMapUnload(const char *mapName)
     {
-        OnMapUnload(mapName);
+        if (OnMapUnload)
+            OnMapUnload(mapName);
     }
 
     const char *GetPluginAuthor();
