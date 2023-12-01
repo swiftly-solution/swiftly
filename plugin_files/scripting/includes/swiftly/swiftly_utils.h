042bfd480c0ba186641cb58a31bee79fc5540d03
skuzzis <andreieurt@gmail.com>
2023-12-01 18:17:23 +0200

    update(scripting): Adding string format

--- plugin_files/scripting/includes/swiftly/swiftly_utils.h
+++ plugin_files/scripting/includes/swiftly/swiftly_utils.h
@@ -39,4 +39,17 @@ uint64_t GetTime()
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
+template <typename... Args>
+const char *format(const std::string &format, Args... args)
+{
+    int size_s = snprintf(nullptr, 0, format.c_str(), args...) + 1;
+    if (size_s <= 0)
+        return "";
+
+    size_t size = static_cast<size_t>(size_s);
+    std::unique_ptr<char[]> buf(new char[size]);
+    snprintf(buf.get(), size, format.c_str(), args...);
+    return std::string(buf.get(), buf.get() + size - 1).c_str();
+}
+
 #endif
