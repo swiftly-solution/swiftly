3765437365a47748a6e59d4168fd0a8a41230a9d
skuzzis <andreieurt@gmail.com>
2023-12-14 20:13:44 +0200

    fix(scripting/translation): Sometimes returned unicode characters

--- src/components/Plugins/src/scripting/Translation.cpp
+++ src/components/Plugins/src/scripting/Translation.cpp
@@ -6,5 +6,9 @@ SMM_API const char *scripting_Translations_Fetch(const char *key)
     if (key == nullptr)
         return nullptr;
 
-    return g_translations->FetchTranslation(key).c_str();
+    std::string translation = g_translations->FetchTranslation(key);
+
+    char *result = new char[translation.size() + 1];
+    strcpy(result, translation.c_str());
+    return result;
 }
