e6bbff56cff43d4afffee6325973a7e74e6b27a2
skuzzis <andreieurt@gmail.com>
2023-12-19 10:53:55 +0200

    fix(NativeHooks): removing fastcall

--- src/hooks/NativeHooks.h
+++ src/hooks/NativeHooks.h
@@ -6,14 +6,14 @@
 class CBasePlayerController;
 class CCommand;
 
-void __fastcall Hook_LoggingSystem_LogDirect(int, int, const char *, ...);
-void __fastcall Hook_LoggingSystem_Log(int, int, const char *, ...);
-void __fastcall Hook_LoggingSystem_LogAssert(const char *, ...);
-void __fastcall Hook_Msg(const char *, ...);
-void __fastcall Hook_Warning(const char *, ...);
-void __fastcall Hook_ClientPrint(CBasePlayerController *, int, const char *, const char *, const char *, const char *, const char *);
-void __fastcall Hook_PrecacheResource(const char *, int64_t);
-bool __fastcall Hook_IsHearingClient(void *, int);
+void Hook_LoggingSystem_LogDirect(int, int, const char *, ...);
+void Hook_LoggingSystem_Log(int, int, const char *, ...);
+void Hook_LoggingSystem_LogAssert(const char *, ...);
+void Hook_Msg(const char *, ...);
+void Hook_Warning(const char *, ...);
+void Hook_ClientPrint(CBasePlayerController *, int, const char *, const char *, const char *, const char *, const char *);
+void Hook_PrecacheResource(const char *, int64_t);
+bool Hook_IsHearingClient(void *, int);
 bool InitializeHooks();
 
 #endif
