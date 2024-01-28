4c9ee202dc7f73b10f98022454b8bc94f25f7820
skuzzis <andreieurt@gmail.com>
2024-01-28 16:38:33 +0200

    update(utils): Moved NextTick to another fire

--- src/components/Plugins/src/language/lua/utils.cpp
+++ src/components/Plugins/src/language/lua/utils.cpp
@@ -9,6 +9,8 @@
 
 void SetupLuaUtils(luacpp::LuaState *state, Plugin *plugin)
 {
+    state->CreateTable("NextTicksFuncToCall");
+
     state->CreateFunction([]() -> const char *
                           { return WIN_LINUX("Windows", "Linux"); },
                           "GetOS");
@@ -21,4 +23,7 @@ void SetupLuaUtils(luacpp::LuaState *state, Plugin *plugin)
     state->CreateFunction([]() -> uint64_t
                           { return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); },
                           "GetTime");
+
+    state->DoString("function NextTick(func) if type(func) ~= \"function\" then return print(\"[Swiftly] The callback needs to be a function.\") end table.insert(NextTicksFuncToCall, func); end");
+    state->DoString("events:on(\"OnGameTick\", function(simulating, first, last) if simulating == 0 then return end; for i=1,#NextTicksFuncToCall do NextTicksFuncToCall[i](); end NextTicksFuncToCall = {}; end)");
 }
