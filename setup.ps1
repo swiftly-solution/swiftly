5a2f7d63e51e689412919170dd6e22a363cc88c4
skuzzis <andreieurt@gmail.com>
2024-02-01 22:35:23 +0200

    update(vendor): Added generated protobuf for cs2

--- setup.ps1
+++ setup.ps1
@@ -3,5 +3,5 @@ $env:HL2SDKCS2 = "$PSScriptRoot\hl2sdk"
 
 mkdir build
 Set-Location build
-python3 ../configure.py --plugin-name=swiftly --plugin-alias=swift -s cs2 --targets=x86_64
+python3 ../configure.py --plugin-name=swiftly --plugin-alias=swift -s cs2 --targets=x86_64 --enable-optimize
 Set-Location ..
