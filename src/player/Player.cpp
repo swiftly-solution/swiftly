0b04116fdf449804b84384d4bdf010dd067871e5
skuzzis <andreieurt@gmail.com>
2024-02-08 19:39:53 +0200

    fix(player): Move Team

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -92,10 +92,7 @@ void Player::SwitchTeam(int team)
     if (!playerController)
         return;
 
-    if (team == CS_TEAM_SPECTATOR)
-        CALL_VIRTUAL(void, g_Offsets->GetOffset("ChangeTeam"), playerController, team);
-    else
-        g_Signatures->FetchSignature<CCSPlayerController_SwitchTeam>("CCSPlayerController_SwitchTeam")(playerController, team);
+    CALL_VIRTUAL(void, g_Offsets->GetOffset("ChangeTeam"), playerController, team);
 }
 
 void Player::Authenticate()
