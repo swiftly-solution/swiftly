ba6441c758a1a2915a1b3dc6a4eaba4a6fd1f914
blu <73272097+blu133721@users.noreply.github.com>
2024-02-15 21:46:51 +0200

    (fix/event): Fixing syntax for OnRoundPostStart

--- plugin_files/scripting/includes/swiftly/gameevents.h
+++ plugin_files/scripting/includes/swiftly/gameevents.h
@@ -40,7 +40,7 @@ void OnPlayerFullUpdate(Player *player, short count) __attribute__((weak));
 void OnItemPickup(Player *player, const char *item, bool silent, long defindex) __attribute__((weak));
 void OnEnterBuyzone(Player *player, bool canbuy) __attribute__((weak));
 void OnExitBuyzone(Player *player, bool canbuy) __attribute__((weak));
-void OnRoundPostStart()__attribute__((weak));
+void OnRoundPostStart() __attribute__((weak));
 void OnEnterBombzone(Player *player, bool hasbomb, bool isplanted) __attribute__((weak));
 void OnExitBombzone(Player *player, bool hasbomb, bool isplanted) __attribute__((weak));
 void OnPlayerFallDamage(Player *player, float damage) __attribute__((weak));
