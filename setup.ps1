861ba536cb531df92eb79fcbc9eea8a260331f0e
skuzzis <andreieurt@gmail.com>
2024-03-11 22:51:46 +0100

    update(builder): Adding XMake

--- setup.ps1
+++ setup.ps1
@@ -4,7 +4,4 @@ if (-not $env:GITHUB_SHA) {
     $env:GITHUB_SHA = (git rev-parse HEAD)
 }
 
-mkdir build
-Set-Location build
-python3 ../configure.py --plugin-name=swiftly --plugin-alias=swift -s cs2 --targets=x86_64 --enable-optimize --hl2sdk-manifests "$PSScriptRoot\hl2sdk-manifests"
-Set-Location ..
+xmake -j 4
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.xmake/profile
+
 export MMSOURCE112="$(pwd)/metamod"
 export HL2SDKCS2="$(pwd)/hl2sdk"
 export HL2SDKMANIFEST="hl2sdk-manifests"
@@ -7,7 +9,5 @@ if [ -z "${GITHUB_SHA}" ]; then
     export GITHUB_SHA=$(git rev-parse HEAD)
 fi
 
-mkdir build
-cd build
-CC=gcc CXX=g++ python3 ../configure.py --plugin-name=swiftly --plugin-alias=swift -s cs2 --targets=x86_64 --enable-optimize --hl2sdk-manifests $HL2SDKMANIFEST
-cd ..
+export XMAKE_ROOT=y
+xmake -j $(nproc)
