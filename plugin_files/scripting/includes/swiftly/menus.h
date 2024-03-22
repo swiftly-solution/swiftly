780d87d281cdb42450678c43b8c7896751b9c30d
skuzzis <andreieurt@gmail.com>
2024-03-22 17:41:58 +0100

    update(plugin_files): Adding debug print

--- plugin_files/scripting/includes/swiftly/menus.h
+++ plugin_files/scripting/includes/swiftly/menus.h
@@ -24,8 +24,15 @@ public:
 
     void RegisterMenu(std::string id, std::string title, std::string color, std::vector<std::pair<std::string, std::string>> options)
     {
-        std::string data = SerializeMenuData(options);
-        REGISTER_METHOD_VOID(scripting_Menus_Register, this->m_plugin_name.c_str(), id.c_str(), title.c_str(), color.c_str(), data.c_str());
+        try
+        {
+            std::string data = SerializeMenuData(options);
+            REGISTER_METHOD_VOID(scripting_Menus_Register, this->m_plugin_name.c_str(), id.c_str(), title.c_str(), color.c_str(), data.c_str());
+        }
+        catch (std::logic_error &e)
+        {
+            print("[Swiftly] [Runtime] An error has occured while trying to register a menu.\n[Swiftly] [Runtime] Error: %s\n", e.what());
+        }
     }
 
     void UnregisterMenu(std::string id)
