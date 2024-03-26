41f1bb802279eb712f950d823475bdabcf7db21a
Andrei Sebastian <61626661+skuzzis@users.noreply.github.com>
2024-03-26 15:46:13 +0200

    fix(scripting/lua/utils): IsLinux

--- src/components/Plugins/src/language/lua/utils.cpp
+++ src/components/Plugins/src/language/lua/utils.cpp
@@ -20,7 +20,7 @@ void SetupLuaUtils(luacpp::LuaState *state, Plugin *plugin)
                           { return WIN_LINUX(true, false); },
                           "IsWindows");
     state->CreateFunction([]() -> bool
-                          { return WIN_LINUX(true, false); },
+                          { return WIN_LINUX(false, true); },
                           "IsLinux");
     state->CreateFunction([]() -> uint64_t
                           { return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); },
