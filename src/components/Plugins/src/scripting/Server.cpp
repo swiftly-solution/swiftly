f0d12db72c3d46a6e416fce71b4f6b91f409d634
blu <73272097+blu133721@users.noreply.github.com>
2024-03-27 21:55:53 +0200

    feat(src): Adding SetQueuedMatchmakingMode

--- src/components/Plugins/src/scripting/Server.cpp
+++ src/components/Plugins/src/scripting/Server.cpp
@@ -83,3 +83,11 @@ SMM_API bool scripting_Server_GetQueuedMatchmaking()
 
     return g_pGameRules->m_bIsQueuedMatchmaking();
 }
+
+SMM_API int scripting_Server_SetQueuedMatchmakingMode(int mode)
+{
+    if (!g_pGameRules)
+        return;
+
+    g_pGameRules->m_nQueuedMatchmakingMode = mode;
+}
