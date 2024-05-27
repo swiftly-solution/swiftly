8d3b7c58d475e928522d7dcb11d6658bb8c5a5c1
skuzzis <andreieurt@gmail.com>
2024-05-27 21:48:40 +0000

    update(builder): AMBuild

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,6 @@
-$env:MMSOURCE112 = "$PSScriptRoot\alliedmodders\metamod"
+$env:MMSOURCE20 = "$PSScriptRoot\alliedmodders\metamod"
 $env:HL2SDKCS2 = "$PSScriptRoot\alliedmodders\hl2sdk"
+$env:HL2SDKMANIFESTS="alliedmodders\hl2sdk-manifests"
 if (-not $env:GITHUB_SHA) {
     $env:GITHUB_SHA = (git rev-parse HEAD)
 }
--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-source ~/.xmake/profile
-
-export MMSOURCE112="$(pwd)/alliedmodders/metamod"
+export MMSOURCE20="$(pwd)/alliedmodders/metamod"
 export HL2SDKCS2="$(pwd)/alliedmodders/hl2sdk"
+export HL2SDKMANIFESTS="/alliedmodders/hl2sdk-manifests"
 
 if [ -z "${GITHUB_SHA}" ]; then
     export GITHUB_SHA=$(git rev-parse HEAD)
 fi
 
-export XMAKE_ROOT=y
-xmake -j $(nproc) -y
+if [ ! -d build ]; then
+    mkdir build
+    cd build
+    CC=gcc CXX=g++ python ../configure.py --enable-optimize --symbol-files -s cs2
+    cd ..
+fi
+
+cd build
+ambuild
+cd ..
