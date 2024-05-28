4a40bd97a9da6d3283e18b9ba6080d333e7aa0db
skuzzis <andreieurt@gmail.com>
2024-05-28 15:23:43 +0000

    update(core/scripting): Macro

--- src/plugins/lua/scripting/core.h
+++ src/plugins/lua/scripting/core.h
@@ -5,6 +5,7 @@
 #include "../../../entrypoint.h"
 
 #define FetchPluginName(state) luabridge::getGlobal(state, "plugin_name").tostring()
+#define FetchPlugin(state) g_pluginManager->FetchPlugin(FetchPluginName(state))
 
 void SetupLuaTypes(LuaPlugin *plugin, lua_State *state);
 void SetupLuaTranslations(LuaPlugin *plugin, lua_State *state);
--- src/plugins/lua/scripting/translations.cpp
+++ src/plugins/lua/scripting/translations.cpp
@@ -4,5 +4,5 @@ void SetupLuaTranslations(LuaPlugin *plugin, lua_State *state)
 {
     luabridge::getGlobalNamespace(state)
         .addFunction("FetchTranslation", +[](std::string key, lua_State *L) -> std::string
-                     { return scripting_FetchTranslation(g_pluginManager->FetchPlugin(FetchPluginName(L)), key); });
+                     { return scripting_FetchTranslation(FetchPlugin(L), key); });
 }
