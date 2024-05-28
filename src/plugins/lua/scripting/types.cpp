a279130e181490d3f0193afe985862828070d46c
skuzzis <andreieurt@gmail.com>
2024-05-28 14:49:16 +0000

    feat(scripting/types): LogType_t

--- src/plugins/lua/scripting/types.cpp
+++ src/plugins/lua/scripting/types.cpp
@@ -1,10 +1,20 @@
 #include "core.h"
 
+#include "../../../types/LogType.h"
+
 void SetupLuaTypes(LuaPlugin *plugin, lua_State *state)
 {
     luabridge::getGlobalNamespace(state)
+
         .beginNamespace("PluginState_t")
         .addConstant("Started", (uint64_t)PluginState_t::Started)
         .addConstant("Stopped", (uint64_t)PluginState_t::Stopped)
+        .endNamespace()
+
+        .beginNamespace("LogType_t")
+        .addConstant("Debug", (uint64_t)LogType_t::Debug)
+        .addConstant("Warning", (uint64_t)LogType_t::Warning)
+        .addConstant("Error", (uint64_t)LogType_t::Error)
+        .addConstant("Common", (uint64_t)LogType_t::Common)
         .endNamespace();
 }
