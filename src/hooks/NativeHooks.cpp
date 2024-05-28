c7006ec011da827f7cd011a4218e3d35dead7751
skuzzis <andreieurt@gmail.com>
2024-05-28 09:47:39 +0000

    chore(hooks): Adding prints

--- src/hooks/NativeHooks.cpp
+++ src/hooks/NativeHooks.cpp
@@ -1,6 +1,5 @@
 #include "NativeHooks.h"
 
-CUtlVector<FuncHookBase *> g_vecHooks;
 CUtlVector<FuncHookBase *> g_funcHooks;
 
 bool InitializeHooks()
@@ -9,10 +8,12 @@ bool InitializeHooks()
 
     FOR_EACH_VEC(g_vecHooks, i)
     {
+        PLUGIN_PRINTF("Hooks", "Hooking %s...\n", g_vecHooks[i]->GetName());
         if (!g_vecHooks[i]->Create())
             return false;
 
         g_vecHooks[i]->Enable();
+        PLUGIN_PRINTF("Hooks", "Hooked %s.\n", g_vecHooks[i]->GetName());
     }
 
     return true;
