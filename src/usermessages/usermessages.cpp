b752d542f9941c6a25ca2c04c894f294320ab489
skuzzis <61626661+skuzzis@users.noreply.github.com>
2024-09-10 13:33:05 +0000

    update(usermessages): Optimize event object

--- src/usermessages/usermessages.cpp
+++ src/usermessages/usermessages.cpp
@@ -3,19 +3,21 @@
 
 SH_DECL_HOOK8_void(IGameEventSystem, PostEventAbstract, SH_NOATTRIB, 0, CSplitScreenSlot, bool, int, const uint64*, INetworkMessageInternal*, const CNetMessage*, unsigned long, NetChannelBufType_t)
 
+PluginEvent* umEvent = nullptr;
+
 void UserMessages::Initialize()
 {
+    umEvent = new PluginEvent("core", nullptr, nullptr);
     SH_ADD_HOOK_MEMFUNC(IGameEventSystem, PostEventAbstract, g_pGameEventSystem, this, &UserMessages::PostEvent, false);
 }
 
 void UserMessages::Destroy()
 {
     SH_REMOVE_HOOK_MEMFUNC(IGameEventSystem, PostEventAbstract, g_pGameEventSystem, this, &UserMessages::PostEvent, false);
+    delete umEvent;
 }
 
 void UserMessages::PostEvent(CSplitScreenSlot nSlot, bool bLocalOnly, int nClientCount, const uint64* clients, INetworkMessageInternal* pEvent, const CNetMessage* pData, unsigned long nSize, NetChannelBufType_t bufType)
 {
-    PluginEvent* event = new PluginEvent("core", nullptr, nullptr);
-    g_pluginManager->ExecuteEvent("core", "OnUserMessageSend", encoders::msgpack::SerializeToString({ string_format("%p|%p|%p", pEvent, pData, clients), bufType == BUF_RELIABLE }), event);
-    delete event;
+    g_pluginManager->ExecuteEvent("core", "OnUserMessageSend", encoders::msgpack::SerializeToString({ string_format("%p|%p|%p", pEvent, pData, clients), bufType == BUF_RELIABLE }), umEvent);
 }
