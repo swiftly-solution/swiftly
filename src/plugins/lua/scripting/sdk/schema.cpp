9a708d4f909e536d85919d43e71e543cc1a1cc4f
skuzzis <61626661+skuzzis@users.noreply.github.com>
2024-10-22 11:36:14 +0000

    update(schema): Default Velocity set to 0

--- src/plugins/lua/scripting/sdk/schema.cpp
+++ src/plugins/lua/scripting/sdk/schema.cpp
@@ -764,8 +764,8 @@ void SDKBaseClass::CBaseEntity_TeleportLua(lua_State *L)
     auto pos = luabridge::LuaRef::fromStack(L, 2);
     auto ang = luabridge::LuaRef::fromStack(L, 3);
     auto vel = luabridge::LuaRef::fromStack(L, 3);
-    Vector po, ve;
-    QAngle an;
+    Vector po(0.0,0.0,0.0), ve(0.0,0.0,0.0);
+    QAngle an(0.0,0.0,0.0);
     if(pos.isInstance<Vector>())
         po = pos.cast<Vector>();
     if(ang.isInstance<QAngle>())
