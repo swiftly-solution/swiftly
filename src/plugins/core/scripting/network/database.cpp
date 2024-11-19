5dd675b7a99c16123eb363c9e438b30099711878
skuzzis <andreieurt@gmail.com>
2024-11-19 22:49:59 +0000

    feat(database): Fallback to default

--- src/plugins/core/scripting/network/database.cpp
+++ src/plugins/core/scripting/network/database.cpp
@@ -129,6 +129,12 @@ PluginDatabase::PluginDatabase(std::string m_connection_name)
 {
     this->connection_name = m_connection_name;
     this->db = g_dbManager->GetDatabase(this->connection_name);
+    
+    if(!this->db) {
+        PRINTF("Database connection \"%s\" doesn't exists inside the database configurations. Automatically falling back to \"default_connection\".\n", m_connection_name.c_str());
+        this->db = g_dbManager->GetDatabase("default_connection");
+    }
+
     if (!this->db)
         this->dbConnected = false;
     else
