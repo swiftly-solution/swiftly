0d57f491a4c364e3f1fce3387894b66d395152c0
skuzzis <andreieurt@gmail.com>
2024-12-15 14:19:51 +0000

    update(database): IQueryBuilder

--- src/network/database/IQueryBuilder.h
+++ src/network/database/IQueryBuilder.h
@@ -12,27 +12,27 @@ class IQueryBuilder;
 class IQueryBuilder
 {
 public:
-    virtual IQueryBuilder* Table(std::string tableName) = 0;
-    virtual IQueryBuilder* Create(std::unordered_map<std::string, std::string> columns) = 0;
-    virtual IQueryBuilder* Alter(std::map<std::string, std::string> columns) = 0;
-    virtual IQueryBuilder* Drop() = 0;
+    virtual void Table(std::string tableName) = 0;
+    virtual void Create(std::unordered_map<std::string, std::string> columns) = 0;
+    virtual void Alter(std::map<std::string, std::string> columns) = 0;
+    virtual void Drop() = 0;
 
-    virtual IQueryBuilder* Select(std::vector<std::string> columns) = 0;
-    virtual IQueryBuilder* Insert(std::map<std::string, std::string> data) = 0;
-    virtual IQueryBuilder* Update(std::map<std::string, std::string> data) = 0;
-    virtual IQueryBuilder* Delete() = 0;
+    virtual void Select(std::vector<std::string> columns) = 0;
+    virtual void Insert(std::map<std::string, std::string> data) = 0;
+    virtual void Update(std::map<std::string, std::string> data) = 0;
+    virtual void Delete() = 0;
 
-    virtual IQueryBuilder* Where(std::string column, std::string operator_, std::string value) = 0;
-    virtual IQueryBuilder* OrWhere(std::string column, std::string operator_, std::string value) = 0;
-    virtual IQueryBuilder* Join(std::string table, std::string onCondition, std::string joinType = "INNER") = 0;
-    virtual IQueryBuilder* OrderBy(std::vector<std::pair<std::string, std::string>> columns) = 0;
-    virtual IQueryBuilder* Limit(int count) = 0;
-    virtual IQueryBuilder* GroupBy(std::vector<std::string> columns) = 0;
-    virtual IQueryBuilder* OnDuplicate(std::map<std::string, std::string> data) = 0;
-    virtual IQueryBuilder* Having(std::string condition) = 0;
-    virtual IQueryBuilder* Distinct() = 0;
-    virtual IQueryBuilder* Offset(int count) = 0;
-    virtual IQueryBuilder* Union(std::string query, bool all) = 0;
+    virtual void Where(std::string column, std::string operator_, std::string value) = 0;
+    virtual void OrWhere(std::string column, std::string operator_, std::string value) = 0;
+    virtual void Join(std::string table, std::string onCondition, std::string joinType = "INNER") = 0;
+    virtual void OrderBy(std::vector<std::pair<std::string, std::string>> columns) = 0;
+    virtual void Limit(int count) = 0;
+    virtual void GroupBy(std::vector<std::string> columns) = 0;
+    virtual void OnDuplicate(std::map<std::string, std::string> data) = 0;
+    virtual void Having(std::string condition) = 0;
+    virtual void Distinct() = 0;
+    virtual void Offset(int count) = 0;
+    virtual void Union(std::string query, bool all) = 0;
 
     virtual std::any PrepareQuery() = 0;
 };
