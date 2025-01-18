a7a03b01caceabaf4b87d6824033ae7562fceee9
skuzzis <andreieurt@gmail.com>
2025-01-18 22:40:10 +0000

    update(vgui/screentext): Default Font

--- src/engine/vgui/ScreenText.h
+++ src/engine/vgui/ScreenText.h
@@ -27,7 +27,7 @@ public:
     ScreenText();
     ~ScreenText();
 
-    void Create(Color color, std::string font = "Verdana", int size = 35);
+    void Create(Color color, std::string font = "Sans Serif", int size = 35);
     void SetupViewForPlayer(Player* player);
     void SetText(std::string text);
     void SetPosition(float posX = 0.0, float posY = 0.0);
