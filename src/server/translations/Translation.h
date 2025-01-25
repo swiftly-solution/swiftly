c9f8bf5534fad707dd5cc18c70e1b938bc6c2ba4
skuzzis <andreieurt@gmail.com>
2025-01-25 10:14:57 +0000

    update(translation): Separate source from header

--- src/server/translations/Translation.h
+++ src/server/translations/Translation.h
@@ -10,23 +10,8 @@ private:
     std::map<std::string, std::string> m_values;
 
 public:
-    Translation() {}
-
-    void RegisterLanguage(std::string lang, std::string value)
-    {
-        if (this->m_values.find(lang) != this->m_values.end())
-            return;
-
-        this->m_values.insert(std::make_pair(lang, value));
-    }
-
-    std::string FetchLanguage(std::string lang)
-    {
-        if (this->m_values.find(lang) == this->m_values.end())
-            return "NO_TRANSLATION";
-
-        return this->m_values[lang];
-    }
+    void RegisterLanguage(std::string lang, std::string value);
+    std::string FetchLanguage(std::string lang);
 };
 
 #endif
--- src/tools/crashreporter/RegisterCallStack.cpp
+++ src/tools/crashreporter/RegisterCallStack.cpp
@@ -6,6 +6,7 @@ RegisterCallStack::RegisterCallStack(std::string plugin_name, std::string stack_
     this->plugin_name = plugin_name;
     this->id = g_callStack->RegisterPluginCallstack(plugin_name, stack_msg);
 }
+
 RegisterCallStack::~RegisterCallStack()
 {
     g_callStack->UnregisterPluginCallstack(plugin_name, this->id);
