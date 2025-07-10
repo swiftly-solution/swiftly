f8d3f7d8f2f304d0519b6e0b88c1695c40392aa5
skuzzis <andreieurt@gmail.com>
2025-07-10 21:02:57 +0000

    feat(docs): Translation

---
+++ documentation/examples/translation/fetchtranslation/example.js
@@ -0,0 +1,17 @@
+commands.Register("credits", (playerid, args, argc, silent, prefix) => {
+    let player = GetPlayer(playerid)
+    if (!player || !player.IsValid()) return;
+    let credits = 50
+    player.SendMsg(MessageType.Chat, FetchTranslation("first_plugin.credits", playerid).replace("{credits}", String(credits)))
+});
+
+/*
+Translation File: addons / swiftly / translations / translation.first_plugin.json
+
+{
+    "credits": {
+        "en": "Your current credits are {credits}!",
+        "ro": "In momentul asta ai {credits} credite!"
+    }
+}
+*/
