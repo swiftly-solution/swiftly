ebe3b8667d68c9995345a1effe4baecb86c6c0a6
skuzzis <andreieurt@gmail.com>
2025-08-11 11:05:29 +0000

    fix(workflows): What to patch

--- generators/docsgen/minifier.js
+++ generators/docsgen/minifier.js
@@ -1,4 +1,4 @@
 const { readFileSync, writeFileSync } = require("fs")
 
 const file = "sdk.json"
-writeFileSync(file, JSON.stringify(JSON.stringify(JSON.parse(readFileSync(file)))))
+writeFileSync(file, JSON.stringify(JSON.parse(readFileSync(file))))
