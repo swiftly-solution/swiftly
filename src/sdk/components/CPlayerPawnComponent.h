6a01d9de0f1636f07e6bb8ab01f31ed735b4e7cc
skuzzis <andreieurt@gmail.com>
2025-08-15 05:28:55 +0000

    update(CPlayerPawnComponent): Method to get pawn

--- src/sdk/components/CPlayerPawnComponent.h
+++ src/sdk/components/CPlayerPawnComponent.h
@@ -1,15 +1,28 @@
+#include "../schema.h"
 
-class CBasePlayerPawn
-{
-};
+class CEntityInstance;
 
 class CPlayerPawnComponent
 {
-public:
     virtual ~CPlayerPawnComponent() = 0;
+    virtual void unk_01() = 0;
+    virtual void unk_02() = 0;
+    virtual void unk_03() = 0;
+    virtual void unk_04() = 0;
+    virtual void unk_05() = 0;
+    virtual void unk_06() = 0;
+    virtual void unk_07() = 0;
+    virtual void unk_08() = 0;
+    virtual void unk_09() = 0;
+    virtual void unk_10() = 0;
+    virtual void unk_11() = 0;
+    virtual void unk_12() = 0;
+    virtual void unk_13() = 0;
+    virtual void unk_14() = 0;
+    virtual void unk_15() = 0;
+    virtual void unk_16() = 0;
+    virtual void unk_17() = 0;
 
-private:
-    [[maybe_unused]] unsigned char __pad0008[0x28]; // 0x8
 public:
-    CBasePlayerPawn* m_pPawn; // 0x30
+    CEntityInstance* GetPawn() { return reinterpret_cast<CEntityInstance*>(schema::GetProp<CNetworkVarChainer>(this, "CPlayerPawnComponent", "__m_pChainEntity").m_pEntity); }
 };
