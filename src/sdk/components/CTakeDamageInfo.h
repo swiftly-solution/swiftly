e724a2885b5f2c33d05ae9df2d3f593f58defcf8
skuzzis <andreieurt@gmail.com>
2025-08-15 05:28:43 +0000

    update(CTakeDamageInfo): Remove damage absorbed

--- src/sdk/components/CTakeDamageInfo.h
+++ src/sdk/components/CTakeDamageInfo.h
@@ -7,48 +7,49 @@ typedef void (*CTakeDamageInfo_Constructor)(void*, void*, void*, void*, Vector*,
 
 enum DDamageTypes_t : uint32_t
 {
-	DMG_GENERIC = 0x0,
-	DMG_CRUSH = 0x1,
-	DMG_BULLET = 0x2,
-	DMG_SLASH = 0x4,
-	DMG_BURN = 0x8,
-	DMG_VEHICLE = 0x10,
-	DMG_FALL = 0x20,
-	DMG_BLAST = 0x40,
-	DMG_CLUB = 0x80,
-	DMG_SHOCK = 0x100,
-	DMG_SONIC = 0x200,
-	DMG_ENERGYBEAM = 0x400,
-	DMG_DROWN = 0x4000,
-	DMG_POISON = 0x8000,
-	DMG_RADIATION = 0x10000,
-	DMG_DROWNRECOVER = 0x20000,
-	DMG_ACID = 0x40000,
-	DMG_PHYSGUN = 0x100000,
-	DMG_DISSOLVE = 0x200000,
-	DMG_BLAST_SURFACE = 0x400000,
-	DMG_BUCKSHOT = 0x1000000,
-	DMG_LASTGENERICFLAG = 0x1000000,
-	DMG_HEADSHOT = 0x2000000,
-	DMG_DANGERZONE = 0x4000000,
+	DMG_GENERIC = 0,
+	DMG_CRUSH = 1 << 0,
+	DMG_BULLET = 1 << 1,
+	DMG_SLASH = 1 << 2,
+	DMG_BURN = 1 << 3,
+	DMG_VEHICLE = 1 << 4,
+	DMG_FALL = 1 << 5,
+	DMG_BLAST = 1 << 6,
+	DMG_CLUB = 1 << 7,
+	DMG_SHOCK = 1 << 8,
+	DMG_SONIC = 1 << 9,
+	DMG_ENERGYBEAM = 1 << 10,
+	DMG_BUCKSHOT = 1 << 11,
+	DMG_DROWN = 1 << 14,
+	DMG_POISON = 1 << 15,
+	DMG_RADIATION = 1 << 16,
+	DMG_DROWNRECOVER = 1 << 17,
+	DMG_ACID = 1 << 18,
+	DMG_PHYSGUN = 1 << 20,
+	DMG_DISSOLVE = 1 << 21,
+	DMG_BLAST_SURFACE = 1 << 22,
+	DMG_HEADSHOT = 1 << 23,
 };
 
 enum TTakeDamageFlags_t : uint32_t
 {
-	DFLAG_NONE = 0x0,
-	DFLAG_SUPPRESS_HEALTH_CHANGES = 0x1,
-	DFLAG_SUPPRESS_PHYSICS_FORCE = 0x2,
-	DFLAG_SUPPRESS_EFFECTS = 0x4,
-	DFLAG_PREVENT_DEATH = 0x8,
-	DFLAG_FORCE_DEATH = 0x10,
-	DFLAG_ALWAYS_GIB = 0x20,
-	DFLAG_NEVER_GIB = 0x40,
-	DFLAG_REMOVE_NO_RAGDOLL = 0x80,
-	DFLAG_SUPPRESS_DAMAGE_MODIFICATION = 0x100,
-	DFLAG_ALWAYS_FIRE_DAMAGE_EVENTS = 0x200,
-	DFLAG_RADIUS_DMG = 0x400,
-	DMG_LASTDFLAG = 0x400,
-	DFLAG_IGNORE_ARMOR = 0x800,
+	DFLAG_NONE = 0,
+	DFLAG_SUPPRESS_HEALTH_CHANGES = 1 << 0,
+	DFLAG_SUPPRESS_PHYSICS_FORCE = 1 << 1,
+	DFLAG_SUPPRESS_EFFECTS = 1 << 2,
+	DFLAG_PREVENT_DEATH = 1 << 3,
+	DFLAG_FORCE_DEATH = 1 << 4,
+	DFLAG_ALWAYS_GIB = 1 << 5,
+	DFLAG_NEVER_GIB = 1 << 6,
+	DFLAG_REMOVE_NO_RAGDOLL = 1 << 7,
+	DFLAG_SUPPRESS_DAMAGE_MODIFICATION = 1 << 8,
+	DFLAG_ALWAYS_FIRE_DAMAGE_EVENTS = 1 << 9,
+	DFLAG_RADIUS_DMG = 1 << 10,
+	DFLAG_FORCEREDUCEARMOR_DMG = 1 << 11,
+	DFLAG_SUPPRESS_INTERRUPT_FLINCH = 1 << 12,
+	DFLAG_IGNORE_DESTRUCTIBLE_PARTS = 1 << 13,
+	DFLAG_IGNORE_ARMOR = 1 << 14,
+	DFLAG_SUPPRESS_UTILREMOVE = 1 << 15,
 };
 
 struct AttackerInfo_t
@@ -57,7 +58,7 @@ struct AttackerInfo_t
 	bool m_bIsPawn;
 	bool m_bIsWorld;
 	CHandle<CEntityInstance> m_hAttackerPawn;
-	uint16_t m_nAttackerPlayerSlot;
+	int32_t m_nAttackerPlayerSlot;
 	int m_iTeamChecked;
 	int m_nTeam;
 };
@@ -105,7 +106,6 @@ private:
 	[[maybe_unused]] uint8_t _x6e[0x2];
 
 public:
-	float m_flDamageAbsorbed;
 	CGameTrace* m_pTrace;
 	TTakeDamageFlags_t m_nDamageFlags;
 	int32_t m_nNumObjectsPenetrated;
